86d1f339bd4fe4a8b77eb2cbd455e1ac1c41d9a4
mohdraid1990 <105244091+mohdraid1990@users.noreply.github.com>
2024-11-15 21:30:12 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/mohdraid1990/' : '/',  
+  base: process.env.NODE_ENV === 'production' ? '/Softline-vueJS/' : '/',
 })
